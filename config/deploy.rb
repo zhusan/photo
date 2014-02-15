8cd933ccdd2d7bd852640769f63305bff89b19ee
zhusan <zhusan333@163.com>
2014-02-15 15:53:40 +0800

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,59 +10,41 @@ set :application, "photo"
 set :scm, :git
 set :repository,  "git@github.com:zhusan/photo.git"
 
+gem 'capistrano-rbenv'
 set :branch, "master"
 set :stage, :production
 set :rails_env, "production"
 set :user, "zs"
 set :use_sudo, false
+gem 'capistrano-rbenv'
+gem 'capistrano-rbenv'
 set :deploy_to, "/home/zs/workspace/#{application}"
 set :app_server, :puma
+gem 'capistrano-rbenv'
 set :normalize_asset_timestamps, false
 
 role :web, "zhusan.net"
 role :app, "zhusan.net"
 role :db,  "zhusan.net", :primary => true
 
-after 'deploy:finalize_update', 'deploy:symlink_db', 'private_pub:restart'
+
 
 namespace :deploy do
-  desc 'Symlinks the database.yml and uploads'
+  desc "cause Passenger to initiate a restart"
 
-  task :symlink_db, :roles => :app do
-    run "ln -nfs #{deploy_to}/shared/database.yml #{release_path}/config/database.yml"
-    run "ln -nfs #{deploy_to}/shared/public/uploads #{release_path}/public/uploads"
-  end
-end
-
-namespace :private_pub do
-  desc "Start private_pub server"
-  task :start do
-    run "cd #{current_path}; RAILS_ENV=production bundle exec rackup private_pub.ru -s thin -E production -D -P tmp/pids/private_pub.pid"
-  end
- 
-  desc "Stop private_pub server"
-  task :stop do
-    run "cd #{current_path};if [ -f tmp/pids/private_pub.pid ] && [ -e /proc/$(cat tmp/pids/private_pub.pid) ]; then kill -9 `cat tmp/pids/private_pub.pid`; fi"
-  end
- 
-  desc "Restart private_pub server"
   task :restart do
-    find_and_execute_task("private_pub:stop")
-    find_and_execute_task("private_pub:start")
+    run"touch #{current_path}/tmp/restart.txt"
+  end
+
+  desc "reload thedatabase with seed data"
+  task :seed do
+    run"cd#{current_path}; rake db:seed RAILS_ENV=#{rails_env}"
   end
 end
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+after "deploy:update_code", :bundle_install
+desc "install thenecessary prerequisites"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+task :bundle_install, :roles => :app do
+  run"cd#{release_path} && bundle install"
+end
